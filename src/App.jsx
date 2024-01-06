709899b0d432d0359825d856eb31e920a099c44a
Ravi Anand <ravianand291@gmail.com>
2024-01-06 11:01:47 +0530

    Added work, projects and contacts

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
